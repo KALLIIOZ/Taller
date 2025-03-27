7e3b1549111c9215c1f9ecb649294c3803f7351d
Alan <alancastillejos3@gmail.com>
2025-03-27 06:48:31 -0600

    .

--- API/main.py
+++ API/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Depends, HTTPException, Query, Path
-from API.routes import users, clientes
+from fastapi import FastAPI
+from API.routes import users, clientes, vehiculos, reparacion, det_rep, piezas
 from fastapi.middleware.cors import CORSMiddleware
 from API.database import create_db_and_tables
 
@@ -14,8 +14,12 @@ app.add_middleware(
 )
 
 
-app.include_router(users.router, prefix="/taller", tags=["users"])
-app.include_router(clientes.router, prefix="/taller", tags=["clientes"])
+app.include_router(users.router, tags=["users"])
+app.include_router(clientes.router, tags=["clientes"])
+app.include_router(vehiculos.router, tags=["vehiculos"])
+app.include_router(reparacion.router, tags=["reparacion"])
+app.include_router(det_rep.router, tags=["det_rep"])
+app.include_router(piezas.router, tags=["piezas"])
 
 @app.on_event("startup")
 def on_startup():
