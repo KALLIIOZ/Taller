880170cb6971fba70726ced5e1d94dc4f4d8cd7f
Alan <alancastillejos3@gmail.com>
2025-03-28 03:14:27 -0600

    Added auth router to main API application

--- API/main.py
+++ API/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from API.routes import users, clientes, vehiculos, reparacion, det_rep, piezas
+from API.routes import users, clientes, vehiculos, reparacion, det_rep, piezas, auth
 from fastapi.middleware.cors import CORSMiddleware
 from API.database import create_db_and_tables
 
@@ -20,6 +20,7 @@ app.include_router(vehiculos.router, prefix="/taller", tags=["vehiculos"])
 app.include_router(reparacion.router, prefix="/taller", tags=["reparacion"])
 app.include_router(det_rep.router, prefix="/taller", tags=["det_rep"])
 app.include_router(piezas.router, prefix="/taller", tags=["piezas"])
+app.include_router(auth.router, prefix="/taller", tags=["auth"])
 
 @app.on_event("startup")
 def on_startup():
