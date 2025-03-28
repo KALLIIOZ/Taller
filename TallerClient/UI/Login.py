9cb70adb9347922bec314a3af42af992cb22caff
Alan <alancastillejos3@gmail.com>
2025-03-28 05:20:11 -0600

    Enhance Login function to include user input fields for username and password

--- TallerClient/UI/Login.py
+++ TallerClient/UI/Login.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 from tkinter import ttk
 
-def Login():
-    pass
+def Login(frame):
+    ttk.Label(frame, text="Usuario").place(x=300, y=300)
+    entry_user_auth = ttk.Entry(frame)
+    entry_user_auth.place(x=300, y=330)
+
+    ttk.Label(frame, text="Contraseña").place(x=300, y=360)
+    entry_pass_auth = ttk.Entry(frame)
+    entry_pass_auth.place(x=300, y=390)
